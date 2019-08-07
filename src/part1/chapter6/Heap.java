72c8daa7cb974ed50ff7d94ba78751b5eb479aa2
dsx <1113553107@qq.com>
2019-08-07 12:52:11 +0800

    chapter 6

---
+++ src/part1/chapter6/Heap.java
@@ -0,0 +1,23 @@
+package part1.chapter6;
+
+public class Heap {
+    public int[] A = new int[0];
+    public int heapSize = 0;
+
+    public Heap(int[] A, int heapSize){
+        this.A = A;
+        this.heapSize = heapSize;
+    }
+
+    public int right(int i){
+        return 2 * i + 1;
+    }
+
+    public int left(int i){
+        return 2 * i;
+    }
+
+    public int parent(int i){
+        return i / 2;
+    }
+}
