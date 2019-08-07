b71a3b16cca14293f7631f2347bb4461d38c57c9
dsx <1113553107@qq.com>
2019-08-07 15:14:22 +0800

    section 6.3

---
+++ src/part1/chapter6/section6_3/BuildMaxHeap.java
@@ -0,0 +1,28 @@
+package part1.chapter6.section6_3;
+
+import part1.chapter6.Heap;
+import part1.chapter6.section6_2.MaxHeapify;
+
+public class BuildMaxHeap {
+    public static void buildMaxHeap(Heap heap){
+        for(int i=heap.heapSize/2;i>=1;i--){
+            MaxHeapify.maxHeapify(heap, i);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] A = new int[10];
+        for(int i=0;i<A.length;i++){
+            A[i] = (int)(Math.random()*100);
+            System.out.print(A[i]+" ");
+        }
+        System.out.println();
+
+        Heap heap = new Heap(A, A.length);
+        buildMaxHeap(heap);
+
+        for (int a : A){
+            System.out.print(a+" ");
+        }
+    }
+}
